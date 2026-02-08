122d401a9c4347d5dcff56196851307c6f27ccaa
Mark Fernando <mark93lk@gmail.com>
2026-02-08 09:38:55 +0200

    index.js modified to extract keys

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const device_pb = require("@chirpstack/chirpstack-api/api/device_pb")
 require("dotenv").config()
 
 const app = express()
+const fs = require("fs");
+const NVS_FILE = "/app/nvs_data.json"; // Path to the NVS data file for container
+//const NVS_FILE = path.resolve(__dirname, "nvs_data.json"); // path to the NVS data file without container
 
 class LoRaDevice {
     #deveui = ""
@@ -79,7 +82,14 @@ app.get("/keygen", async (req, res) => {
     
         await device.GenerateDevice(device_deveui, device_name)    
         const key = await device.GenerateKeys();
-    
+
+        const nvsData = {
+            devEui: device_deveui,
+            joinEui: "8000000000000006",
+            appKey: key
+        };
+
+        fs.writeFileSync(NVS_FILE, JSON.stringify(nvsData, null, 4));
         const js = `
             <script>
                 if (confirm("KEY: ${key}")) {
@@ -89,10 +99,48 @@ app.get("/keygen", async (req, res) => {
             </script>
         `
         res.send(js)
+
     } catch (e) {
         res.send(e)
     }
 })
+
+// defined a route to get the latest device information
+app.get("/api/latest-device", (req, res) => {
+    try {
+        if (fs.existsSync(NVS_FILE)) {
+            const data = fs.readFileSync(NVS_FILE, 'utf8');
+            res.json(JSON.parse(data));
+        } else {
+            res.status(404).json({ error: "No device has been generated yet." });
+        }
+    } catch (err) {
+        res.status(500).json({ error: "Failed to read key file." });
+    }
+});
+
+// example python code to get the keys from the given route. 
+/*
+import requests
+import serial
+import json
+
+data = requests.get("http://localhost:3000/api/latest-device").json() // json format data (deveui, joineui and appkey)
+
+app = serial.Serial('/dev/ttyUSB0', 9600) 
+app.write(json.dumps(data).encode())
+app.close()
+
+*/
+
+// NVS c++
+// esp side needs to use uart to get the data
+
+/*
+
+
+
+*/
 
 app.listen(3000, () => {
     console.log("Listening on port: 3000")
